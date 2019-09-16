ce551694b8f607396578431846624c833d6a0fda
George <starcalluh@live.com>
2019-09-16 18:49:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
-# My Code here....
+ def map_to_negativize(source_array)
+ source_array = [1,2,3,9]
+  p source_array.map { |n| n * -1}
+end
+  
+def map_to_no_change(dune)
+dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
+dune.each
+end
+
+
+
